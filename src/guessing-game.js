25aed3583d9b3b52ec9a5e92aec6b446db187887
anna-left <87468994+anna-left@users.noreply.github.com>
2021-08-25 17:04:41 +0300

    feat: add functions

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,22 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        let res = Math.ceil((this.min + this.max) / 2);
+        this.try = res;
+        return res;
     }
 
     lower() {
-
+        this.max = this.try;
     }
 
     greater() {
-
+        this.min = this.try;
     }
 }
 
